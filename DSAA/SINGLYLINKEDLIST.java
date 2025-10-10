302c78ce7b2afca716124535f68cd280d5ed6f68
mayur sonwane <mayurperks@gmail.com>
2025-10-10 11:44:06 +0530

    fix: code

--- DSAA/SINGLYLINKEDLIST.java
+++ DSAA/SINGLYLINKEDLIST.java
@@ -5,12 +5,13 @@ public class SINGLYLINKEDLIST {
 
         public Node(int data){
             this.data = data;
-            this.next = next;
+            this.next = null;
         }
     }
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data){
         Node newNode  = new Node(data);
@@ -18,6 +19,7 @@ public class SINGLYLINKEDLIST {
             head = tail = newNode;
             return;
         }
+        size++;
         newNode.next = head;
         head = newNode;
     }
@@ -28,6 +30,7 @@ public class SINGLYLINKEDLIST {
             head=tail=newNode;
             return;
         }
+        size++;
         tail.next = newNode;
         tail = newNode;
     }
@@ -50,6 +53,7 @@ public class SINGLYLINKEDLIST {
             return;
         }
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
         while(i<idx-1){
@@ -59,6 +63,105 @@ public class SINGLYLINKEDLIST {
         newNode.next = temp.next;
         temp.next = newNode;
     }
+
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+    public int removeLast(){
+         if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
+        Node prev = head;
+        for(int  i = 0; i<size-2;i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int itrSearch(int key){
+        Node temp = head;
+        int i = 0;
+        while(temp != null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+    public int recSearch(int key){
+        return helper(head,key);
+    }
+    private int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int index = helper(head.next, key);
+        return index == -1 ? -1 : index + 1;
+    }
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail =head ;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        head = prev;
+    }
+
+    public void deleteNthFromEnd(int n){
+        // calculate size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        if(n == sz){
+            head = head.next;
+            return;
+        }
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while(i<iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
     public static void main(String[] args) {
         SINGLYLINKEDLIST list = new SINGLYLINKEDLIST();
         list.addFirst(2);
@@ -66,6 +169,19 @@ public class SINGLYLINKEDLIST {
         list.addLast(4);
         list.addLast(5);
         list.add(2,3);
-        list.print();
+        // list.print();
+    //    System.out.println(list.size);
+    //    list.removeFirst();
+    //    list.print();
+    //        System.out.println(list.size);
+    //        list.removeLast();
+    //        list.print();
+    //                   System.out.println(list.size);
+//   System.out.println(list.itrSearch(4));
+//     System.out.println(list.recSearch(4));
+    // list.reverse();
+    list.print();
+    list.deleteNthFromEnd(2);
+list.print();
     }
 }
